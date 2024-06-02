02c6abd6b5fdc60309c080686a99f1d3105436cb
thekiehlB <thekiehlb@gmail.com>
2024-06-02 16:09:35 +0900

    message

---
+++ packages/config/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./views/**/*.{js,ts,jsx,tsx,mdx}",
+    "./views/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
